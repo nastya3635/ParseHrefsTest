d62edfee8ac2e8a4c73bc62e71afec72125d7742
nastya3635 <nastik3635@gmail.com>
2019-01-27 22:30:25 +0400

    Добавление класса WebRequestHelpers, в котором метод GetContent получает URL адрес, запрашивает исходный код страницы, получает его, преобразует в строку и возвращает её.

---
+++ TestAnastasiyaM/Helpers/WebRequestHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Net;
+
+namespace TestAnastasiyaM.Helpers
+{
+    public class WebRequestHelpers
+    {
+        public static string GetContent(string urladress)
+        {
+            var request = WebRequest.Create(urladress);
+            string content;
+            using (var response = request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        content = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            return content;
+        }
+    }
+}
--- TestAnastasiyaM/Program.cs
+++ TestAnastasiyaM/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Net;
 using System.Text.RegularExpressions;
+using TestAnastasiyaM.Helpers;
 
 namespace TestAnastasiyaM
 {
@@ -11,27 +12,29 @@ namespace TestAnastasiyaM
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите адрес сайта");
+            Console.WriteLine("Пожалуйста, введите url адресс");
             string adress = Console.ReadLine();
-            WebRequest request = WebRequest.Create(adress);
-            WebResponse response = request.GetResponse();
+
+            var content = WebRequestHelpers.GetContent(adress);
+            Console.WriteLine(content);
+            //WebResponse response = request.GetResponse();
             
-            using (Stream stream = response.GetResponseStream())
-            {
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    StreamWriter file = new StreamWriter(Path.GetFullPath(@"..\..\..\")+"TestFile.txt");
-                    string content = reader.ReadToEnd();
-                    var matches = Regex.Matches(content, "<a.+?href=\"(.+?)\"");
-                    for (int i=0; i<20 && i<=matches.Count; i++)
-                    { 
-                        Console.WriteLine(matches[i].Groups[1]);
-                        file.Write(matches[i].Groups[1]+"\n");
-                    }
-                    file.Close();
-                }
-            }
-            response.Close();
+            //using (Stream stream = response.GetResponseStream())
+            //{
+            //    using (StreamReader reader = new StreamReader(stream))
+            //    {
+            //        StreamWriter file = new StreamWriter(Path.GetFullPath(@"..\..\..\")+"TestFile.txt");
+            //        string content = reader.ReadToEnd();
+            //        var matches = Regex.Matches(content, "<a.+?href=\"(.+?)\"");
+            //        for (int i=0; i<20 && i<=matches.Count; i++)
+            //        { 
+            //            Console.WriteLine(matches[i].Groups[1]);
+            //            file.Write(matches[i].Groups[1]+"\n");
+            //        }
+            //        file.Close();
+            //    }
+            //}
+            //response.Close();
             Console.Read();
         }
     }
