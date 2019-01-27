e5d5e2f31e2ee18ec3605865530b36ee201cae18
nastya3635 <nastik3635@gmail.com>
2019-01-27 23:18:22 +0400

    добавление вывода ссылок в консоль. Удаление ненужного кода.

--- TestAnastasiyaM/Program.cs
+++ TestAnastasiyaM/Program.cs
@@ -18,24 +18,10 @@ namespace TestAnastasiyaM
 
             var filePath = FileHelpers.WriteToFileInTempFolder(hrefs, OutputFileName, false);
             Console.WriteLine($"Сылки сохранены в: {filePath}");
-            //WebResponse response = request.GetResponse();
 
-            //using (Stream stream = response.GetResponseStream())
-            //{
-            //    using (StreamReader reader = new StreamReader(stream))
-            //    {
-            //        StreamWriter file = new StreamWriter(Path.GetFullPath(@"..\..\..\")+"TestFile.txt");
-            //        string content = reader.ReadToEnd();
-            //        var matches = Regex.Matches(content, "<a.+?href=\"(.+?)\"");
-            //        for (int i=0; i<20 && i<=matches.Count; i++)
-            //        { 
-            //            Console.WriteLine(matches[i].Groups[1]);
-            //            file.Write(matches[i].Groups[1]+"\n");
-            //        }
-            //        file.Close();
-            //    }
-            //}
-            //response.Close();
+            Console.WriteLine("Hrefs:");
+            hrefs.ForEach(href => Console.WriteLine(href));
+            
             Console.Read();
         }
     }
