66deb57e81e5b35b7540ac26302e9a8c27837815
nastya3635 <nastik3635@gmail.com>
2019-01-27 19:55:30 +0400

    Решение задачи от 25.01.19

--- TestAnastasiyaM/Program.cs
+++ TestAnastasiyaM/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace TestAnastasiyaM
 {
@@ -6,7 +9,28 @@ namespace TestAnastasiyaM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите адрес сайта");
+            string adress = Console.ReadLine();
+            WebRequest request = WebRequest.Create(adress);
+            WebResponse response = request.GetResponse();
+            
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    StreamWriter file = new StreamWriter(Path.GetFullPath(@"..\..\..\")+"TestFile.txt");
+                    string content = reader.ReadToEnd();
+                    var matches = Regex.Matches(content, "<a.+?href=\"(.+?)\"");
+                    for (int i=0; i<20 && i<=matches.Count; i++)
+                    { 
+                        Console.WriteLine(matches[i].Groups[1]);
+                        file.Write(matches[i].Groups[1]+"\n");
+                    }
+                    file.Close();
+                }
+            }
+            response.Close();
+            Console.Read();
         }
     }
 }
